b55e71784178c3996ab37a71d6ef09d16dde628c
shlomog12 <73135976+shlomog12@users.noreply.github.com>
2021-12-12 22:35:58 +0200

    fix conflict

--- app/src/main/java/com/Final/mysalary/DTO/Job.java
+++ app/src/main/java/com/Final/mysalary/DTO/Job.java
@@ -16,9 +16,6 @@ public class Job {
         this.jobName = jobName;
     }
 
-
-
-
     public String getMailWorker() {
         return mailWorker;
     }
@@ -43,7 +40,6 @@ public class Job {
     public void setJobName(String jobName) {
         this.jobName = jobName;
     }
-
     @Override
     public String toString() {
         return "Job{" +
