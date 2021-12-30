6b93babd053dfe26fb39399aa6985b32d49cbfdc
shlomog12 <73135976+shlomog12@users.noreply.github.com>
2021-12-30 23:25:42 +0200

    add Icon

--- app/src/main/java/com/Final/mysalary/Controller/notfication/FcmNotificationsSender.java
+++ app/src/main/java/com/Final/mysalary/Controller/notfication/FcmNotificationsSender.java
@@ -37,12 +37,9 @@ public class FcmNotificationsSender  {
         this.body = body;
         this.mContext = mContext;
         this.mActivity = mActivity;
-
-
     }
 
     public void SendNotifications() {
-
         requestQueue = Volley.newRequestQueue(mActivity);
         JSONObject mainObj = new JSONObject();
         try {
@@ -50,42 +47,26 @@ public class FcmNotificationsSender  {
             JSONObject notiObject = new JSONObject();
             notiObject.put("title", title);
             notiObject.put("body", body);
-            notiObject.put("icon", R.drawable.icon); // enter icon that exists in drawable only
-
-
-
+            notiObject.put("icon", R.drawable.money); // enter icon that exists in drawable only
             mainObj.put("notification", notiObject);
-
-
             JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, postUrl, mainObj, new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response) {
-
-                    // code run is got response
-
                 }
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    // code run is got error
-
                 }
             }) {
                 @Override
                 public Map<String, String> getHeaders() throws AuthFailureError {
-
-
                     Map<String, String> header = new HashMap<>();
                     header.put("content-type", "application/json");
                     header.put("authorization", "key=" + fcmServerKey);
                     return header;
-
-
                 }
             };
             requestQueue.add(request);
-
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
